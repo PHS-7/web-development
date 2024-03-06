48764d6e4300d61e0c66cae54b9b058a4c7c2d84
Pedro Henrique Santos Moreira <moreira.phs@alu.ufc.br>
2024-03-05 22:14:18 -0300

    segunda questao

--- atv00/src/App.js
+++ atv00/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import MeusDados from './components/atividade00/MeusDados';
+import MeusDados2 from './components/atividade00/MeusDados2';
+
 
 function App() {
   return (
-    <MeusDados></MeusDados>
+    <div>
+      <MeusDados2 nome="PedroVitor" curso="ES" uni="UFC"/>
+    </div>
   );
 }
 
---
+++ atv00/src/components/atividade00/MeusDados2.jsx
@@ -0,0 +1,11 @@
+const MeusDados2 = ({ nome, curso, uni }) => {
+    return (
+        <div>
+            <h1>Nome: {nome}</h1>
+            <h1>Curso: {curso}</h1>
+            <h1>Universidade: {uni}</h1>
+        </div>
+    )
+}
+
+export default MeusDados2;
